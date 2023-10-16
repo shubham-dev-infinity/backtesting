03a1ecae2b9584922cafc1e600b1f35905985368
shubham <shubhamsutariya@atyantik.com>
2023-10-16 18:07:26 +0530

    tailwind css added and  responsive

--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -45,19 +45,19 @@ function LandingPage() {
 
     return (
         <>
-            <div className='home_banner_wrpper'>
+            {/* <div className='home_banner_wrpper'>
                 <div className='banner_wrapper'>
                     <div className='container'>
                         <div className='content_mt'>
-                            <div className='row align-items-center'>
-                                <div className='col-lg-7'>
-                                    <h1 className='banner_head position-relative inline'>Let’s Go To your <br />
+                            <div className='flex justify-between items-center'>
+                                <div className='flex_2'>
+                                    <h1 className='banner_head relative inline'>Let’s Go To your <br />
                                         <span>Backtesting</span> Here
                                         <img src='/assets/img/starImg.png' alt='' className='starImg' />
                                     </h1>
                                     <h3 className='banner_grayTxt'>(Banknifty & Nifty) </h3>
                                 </div>
-                                <div className='col-lg-5'>
+                                <div className='flex-1'>
                                     {!isLoggedIn && <div className="card_box">
                                         <h3 className="card_title">Login</h3>
                                         <div className="login_signup">
@@ -86,96 +86,149 @@ function LandingPage() {
                         </div>
                     </div>
                 </div>
+            </div> */}
+            <div className='home_banner_wrpper mb-10 bg-gray-100'>
+                <div className='banner_wrapper'>
+                    <div className='container'>
+                        <div className='content_mt'>
+                            <div className='lg:flex lg:justify-between lg:items-center'>
+                                <div className='lg:flex-2 mb-8 lg:mb-0'>
+                                    <h1 className='banner_head relative inline text-3xl lg:text-5xl font-semibold'>
+                                        Let’s Go To your <br />
+                                        <span>Backtesting</span> Here
+                                        <img src='/assets/img/starImg.png' alt='' className='starImg' />
+                                    </h1>
+                                    <h3 className='banner_grayTxt'>(Banknifty & Nifty) </h3>
+                                </div>
+                                <div className='lg:flex-1'>
+                                    {!isLoggedIn && (
+                                        <div className="card_box bg-white rounded-lg shadow-md p-6 lg:p-10">
+                                            <h3 className="card_title text-2xl text-center font-semibold mb-8">Login</h3>
+                                            <div className="login_signup flex">
+                                                <Button className="primaryBtn flex-1" >Login</Button>
+                                                <Button className="signup_btn flex-1"><Link to='/signup'>Sign Up</Link></Button>
+                                            </div>
+                                            <div className="input_text mt-5">
+                                                <div className="e_mail">
+                                                    <h5 className="text-gray-500 text-base">Email</h5>
+                                                    <input className='inputClass w-full px-4 py-2 border-b border-gray-300' type="email" placeholder="Enter your Email here" name="userId" value={formFields.userId} onChange={handleFormChange} />
+                                                </div>
+                                                <div className="e_mail">
+                                                    <h5 className="text-gray-500 text-base">Password</h5>
+                                                    <input className='inputClass w-full px-4 py-2 border-b border-gray-300' type="password" placeholder="Enter your Email here" name="password" value={formFields.password} onChange={handleFormChange} />
+                                                </div>
+                                                <p className="forgot_text text-right"><Link to='/reset-password'>Forgot Password?</Link> </p>
+                                            </div>
+                                            <div className="main_btn mt-10">
+                                                <Button className="btn_wrap bg-blue-500 text-white w-full rounded-l">Login</Button>
+                                                <p className="btn_wrap_text text-gray-500 text-base my-2">Or</p>
+                                                <p className="btn_wrap_text text-gray-500 text-base mb-10">Don’t have an account? <span className="signup_mark text-blue-500"><Link to={'/signup'}>Sign Up</Link></span></p>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <section>
-                <div className="container">
-                    <div className="about_text">
-                        <div className="text_underline">
-                            <h2 className="under_text">About Backtesting</h2>
-                        </div>
-                        <h5 className="back_testing">Backtesting lets you look at your strategies on chronicled information to decide
-                            how well it would have
-                            worked within the past.
-                            In case you've got created a technique with which you're prepared to go live, the Backtesting highlight
-                            will assist
-                            you to get it in the event that your strategies are reasonable and possibly effective.</h5>
-                        <h5 className="back_testing">Backtesting lets you look at your strategies on chronicled information to decide how well it would have
-                            worked within the past.
-                            In case you've got created a technique with which you're prepared to go live, the Backtesting highlight
-                            will assist
-                            you to get it in the event that your strategies are reasonable and possibly effective.</h5>
-                        <h5 className="back_testing">Backtesting lets you look at your strategies on chronicled information to decide how well it would have
-                            worked within the past.
-                            In case you've got created a technique with which you're prepared to go live, the Backtesting highlight
-                            will assist
-                            you to get it in the event that your strategies are reasonable and possibly effective.</h5>
-                        <h5 className="back_testing">Backtesting lets you look at your strategies on chronicled information to decide how well it would have
-                            worked within the past.
-                            In case you've got created a technique with which you're prepared to go live, the Backtesting highlight
-                            will assist
-                            you to get it in the event that your strategies are reasonable and possibly effective.</h5>
+            <section className="bg-white py-6 md:py-10 lg:py-14">
+                <div className="container mx-auto sm:px-4">
+                    <div className="text-2xl font-semibold relative mb-4">
+                        <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center">
+                            About Backtesting
+                        </h2>
+                        <div className="absolute h-3 w-3/12 bg-blue-500 rounded-full mb-16 mt-2 left-1/2 transform -translate-x-1/2 shadow-md backdrop-blur-md"></div>
+                    </div>
+                    <div className="text-center relative">
+                        <p className="text-base md:text-lg lg:text-xl mt-10">
+                            <h5 className="text-base md:text-lg lg:text-xl mb-4">
+                                Backtesting lets you look at your strategies on historical data to decide how well it would have worked within the past.
+                                In case you've got created a technique with which you're prepared to go live, the Backtesting highlight will assist you to get it in the event that your strategies are reasonable and possibly effective.
+                            </h5>
+                            <h5 className="text-base md:text-lg lg:text-xl mb-4">
+                                Backtesting lets you look at your strategies on historical data to decide how well it would have worked within the past.
+                                In case you've got created a technique with which you're prepared to go live, the Backtesting highlight will assist you to get it in the event that your strategies are reasonable and possibly effective.
+                            </h5>
+                            <h5 className="text-base md:text-lg lg:text-xl mb-4">
+                                Backtesting lets you look at your strategies on historical data to decide how well it would have worked within the past.
+                                In case you've got created a technique with which you're prepared to go live, the Backtesting highlight will assist you to get it in the event that your strategies are reasonable and possibly effective.
+                            </h5>
+                            <h5 className="text-base md:text-lg lg:text-xl mb-4">
+                                Backtesting lets you look at your strategies on historical data to decide how well it would have worked within the past.
+                                In case you've got created a technique with which you're prepared to go live, the Backtesting highlight will assist you to get it in the event that your strategies are reasonable and possibly effective.
+                            </h5>
+                        </p>
                     </div>
                 </div>
             </section>
-            <section>
-                <div className="container">
-                    <div className="feature_line">
-                        <h2 className="feature_txt">Features</h2>
-                    </div>
-                    <div className='row g-5'>
-                        <div className='col-lg-6'>
-                            <img src='/assets/img/feature1.png' alt='star' />
+            <section className="bg-white py-6 md:py-10 lg:py-14">
+                <div className="container mx-auto">
+                    <div className="relative">
+                        <div className="text-2xl font-semibold relative mb-4">
+                            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center">Features</h2>
+                            <div className="absolute h-3 w-4/12 bg-blue-500 rounded-full mb-16 mt-2 left-1/2 transform -translate-x-1/2 shadow-md backdrop-blur-md"></div>
                         </div>
-                        <div className='col-lg-6'>
-                            <h5 className="featureTxt">The app offers a vast collection of safety talks
-                                tailored specifically for the construction and
-                                marine industries. Workers can easily search
-                                and browse through the library to find relevant
-                                topics and materials for their safety briefings.
-                                This app provides a comprehensive solution for
-                                promoting safety awareness and compliance.</h5>
+                        <div className="md:flex lg:flex grid gap-x-8 mt-10">
+                            <div className="md:w-1/2 lg:w-1/2 flex items-center">
+                                <img src="/assets/img/feature1.png" alt="star" className="w-full feature_Img" />
+                            </div>
+                            <div className="md:w-1/2 lg:w-1/2 flex items-center mt-6 md:mt-0 lg:mt-0 px-8">
+                                <h5 className="text-base md:text-lg lg:text-xl mb-4">
+                                    The app offers a vast collection of safety talks tailored specifically for the construction and marine industries. Workers can easily search and browse through the library to find relevant topics and materials for their safety briefings. This app provides a comprehensive solution for promoting safety awareness and compliance.
+                                </h5>
+                            </div>
                         </div>
-                        <div className='col-lg-6'>
-                            <h5 className="featureTxt">The app offers a vast collection of safety talks
-                                tailored specifically for the construction and
-                                marine industries. Workers can easily search
-                                and browse through the library to find relevant
-                                topics and materials for their safety briefings.
-                                This app provides a comprehensive solution for
-                                promoting safety awareness and compliance.</h5>
+                        <div className="md:flex md:flex-row lg:flex mt-6 md:mt-0 lg:mt-0 flex flex-col-reverse">
+                            <div className="md:w-1/2 lg:w-1/2 flex items-center px-8">
+                                <h5 className="text-base md:text-lg lg:text-xl mb-4">
+                                    The app offers a vast collection of safety talks tailored specifically for the construction and marine industries. Workers can easily search and browse through the library to find relevant topics and materials for their safety briefings. This app provides a comprehensive solution for promoting safety awareness and compliance.
+                                </h5>
+                            </div>
+                            <div className="md:w-1/2 lg:w-1/2 flex items-center">
+                                <img src="/assets/img/feature2.png" alt="star" className="w-full feature_Img" />
+                            </div>
                         </div>
-                        <div className='col-lg-6'>
-                            <img src='/assets/img/feature2.png' alt='star' />
-                        </div>
-                        <div className='col-lg-6'></div>
                     </div>
                 </div>
             </section>
-            <section>
-                <div className="container">
-                    <div className="feature_line">
-                        <h2 className="feature_txt">Subscription Plans</h2>
+            <section className="bg-white py-6 md:py-10 lg:py-14">
+                <div className="container mx-auto">
+                    <div className="text-2xl font-semibold relative mb-4">
+                        <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center">Subscription Plans</h2>
+                        <div className="absolute h-3 w-4/12 bg-blue-500 rounded-full mb-16 mt-2 left-1/2 transform -translate-x-1/2 shadow-md backdrop-blur-md"></div>
                     </div>
-                    <div className="grid grid-cols-4 gap-4">
-                        {
-                            subscriptionData.map((sub, index) => <SubscriptionPlan key={index} index={index} activeIndex={activeSub} duration={sub.duration} features={sub.features} price={sub.price} priceLabel={sub.priceLabel} selectSub={setActiveSub} />)
-                        }
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-8">
+                        {subscriptionData.map((sub, index) => (
+                            <SubscriptionPlan
+                                key={index}
+                                index={index}
+                                activeIndex={activeSub}
+                                duration={sub.duration}
+                                features={sub.features}
+                                price={sub.price}
+                                priceLabel={sub.priceLabel}
+                                selectSub={setActiveSub}
+                            />
+                        ))}
                     </div>
                 </div>
             </section>
-            <section>
-                <div className="container">
-                    <div className="get_wrap">
-                        <div className="get_underline">
-                            <h2 className="get_text">Get To Know us</h2>
+            <section className="bg-white py-6 md:py-10 lg:py-14">
+                <div className="container mx-auto">
+                    <div className="relative">
+                        <div className="text-2xl font-semibold relative mb-4">
+                            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center">Get To Know us</h2>
+                            <div className="absolute h-3 w-4/12 bg-blue-500 rounded-full mb-16 mt-2 left-1/2 transform -translate-x-1/2 shadow-md backdrop-blur-md"></div>
                         </div>
-                        <h5 className="know_us_text">We are here to serve you in the best possible way. Do let us know your
-                            feedback or your query. We are happy to help you always.</h5>
-                        <div className="know_us">
+                        <h5 className="text-base md:text-lg lg:text-xl mb-10 p-12 text-center">
+                            We are here to serve you in the best possible way. Do let us know your feedback or your query. We are happy to help you always.
+                        </h5>
+                        <div className="flex justify-center mb-10 ">
                             <Button className="email_us" children={'Email Us'} />
                         </div>
-                        <h5 className="email_us_text">Exalgo Infotech Pvt. Ltd.</h5>
-                        <h5 className="email_us_text">Address: Surat, Gujrat, India</h5>
+                        <h5 className="text-base md:text-lg lg:text-xl flex justify-center mb-2">Exalgo Infotech Pvt. Ltd.</h5>
+                        <h5 className="text-base md:text-lg lg:text-xl flex justify-center">Address: Surat, Gujarat, India</h5>
                     </div>
                 </div>
             </section>
